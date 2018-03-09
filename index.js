941832cfe837f02a2294960497c08e15622d69a8
James Thomas <jamesjacobthomas7@gmail.com>
2018-03-09 07:23:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var name = "Susan"
 var height = "70"
 
-if (index.name === "Susan") {
+if (name === "Susan") {
     console.log("The name is correct")
 } else {
     console.log("Expected: Susan, Received: "+index.name)
@@ -9,7 +9,7 @@ if (index.name === "Susan") {
 
 
 
-if (index.height === 70) {
+if (height === 70) {
     console.log("The height is correct")
 } else {
     console.log("Expected: 70, Received: "+index.height)
